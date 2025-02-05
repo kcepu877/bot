74583044808be31d06b1c47afc726a13a90f7e28
kcepu877 <kcepu877@gmail.com>
2025-02-06 06:11:04 +0700

    Update replace_subdomain.py

--- replace_subdomain.py
+++ replace_subdomain.py
@@ -22,24 +22,19 @@ def read_subdomain_from_yaml(yaml_file):
             return data.get('subdomain', None)
     return None
 
-# Fungsi untuk mengganti subdomain di _worker.js
-def replace_subdomain_in_workerjs(workerjs_file, new_subdomain, old_subdomain):
-    with open(workerjs_file, 'r') as file:
-        lines = file.readlines()
+# Fungsi untuk mengganti subdomain di js/_worker.js
+def replace_subdomain_in_worker_js(worker_js_file, new_subdomain, old_subdomain):
+    with open(worker_js_file, 'r') as file:
+        content = file.read()
 
-    updated_lines = []
-    for line in lines:
-        # Hanya mengganti subdomain yang sesuai (contoh: xxx.bmkg.xyz) dengan subdomain baru
-        if old_subdomain in line:
-            line = re.sub(r'\b' + re.escape(old_subdomain) + r'\.bmkg\.xyz', new_subdomain + '.bmkg.xyz', line)
-        updated_lines.append(line)
+    updated_content = re.sub(r'\b' + re.escape(old_subdomain) + r'\.kere\.us\.kg', new_subdomain + '.kere.us.kg', content)
 
-    with open(workerjs_file, 'w') as file:
-        file.writelines(updated_lines)
+    with open(worker_js_file, 'w') as file:
+        file.write(updated_content)
 
 def main():
     yaml_file = 'subdomain.yml'
-    workerjs_file = '_worker.js'
+    worker_js_file = 'js/_worker.js'  # Menggunakan js/_worker.js
     list_file = 'subdomain_list.txt'
 
     # Baca daftar subdomain dari file
@@ -65,8 +60,8 @@ def main():
     next_index = (current_index + 1) % len(subdomain_list)
     next_subdomain = subdomain_list[next_index]
 
-    # Ganti subdomain di _worker.js
-    replace_subdomain_in_workerjs(workerjs_file, next_subdomain, last_subdomain)
+    # Ganti subdomain di js/_worker.js
+    replace_subdomain_in_worker_js(worker_js_file, next_subdomain, last_subdomain)
 
     # Simpan subdomain yang digunakan ke file YAML
     save_subdomain_to_yaml(next_subdomain, yaml_file)
